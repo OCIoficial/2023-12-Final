47d104e654fc40814a46f734f548c68cb1d7178a
Nico Lehmann <nico.lehmannm@gmail.com>
2023-12-01 15:40:35 -0300

    balancing: actualizar solucion con should-fail

--- balancin/solutions/partial/vivillarroel-st1.cpp
+++ balancin/solutions/partial/vivillarroel-st1.cpp
@@ -1,19 +1,19 @@
-// @ocimatic should-pass=[st1]
+// @ocimatic should-fail=[st2, st3]
 // Solución a la subtarea 1
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	long long n, c, g;
-	cin >> n >> c >> g;
-	for (int i = 1; i <= n; ++i) {
-		for (int j = 1; j <=n; ++j) {
-			if (c*i == g*j) {
-				cout << "SI\n";
-				return 0;
-			}
-		}
-	}
-	cout << "NO\n";
-	return 0;
+  long long n, c, g;
+  cin >> n >> c >> g;
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= n; ++j) {
+      if (c * i == g * j) {
+        cout << "SI\n";
+        return 0;
+      }
+    }
+  }
+  cout << "NO\n";
+  return 0;
 }
--- balancin/solutions/partial/vivillarroel-st2.cpp
+++ balancin/solutions/partial/vivillarroel-st2.cpp
@@ -1,18 +1,18 @@
-// @ocimatic should-pass=[st1,st2]
-// Solución a la subtarea 1
+// @ocimatic should-fail=[st3]
+// Solución a la subtarea 2
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	long long n, c, g;
-	cin >> n >> c >> g;
-	for (int i = 1; i <= n; ++i) {
-		long long b = c*i/g;
-		if (b <= n && c*i % g == 0) {
-			cout << "SI\n";
-			return 0;
-		}
-	}
-	cout << "NO\n";
-	return 0;
+  long long n, c, g;
+  cin >> n >> c >> g;
+  for (int i = 1; i <= n; ++i) {
+    long long b = c * i / g;
+    if (b <= n && c * i % g == 0) {
+      cout << "SI\n";
+      return 0;
+    }
+  }
+  cout << "NO\n";
+  return 0;
 }
