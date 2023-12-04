f562de1473109640444045434db0aaec5b6a95f4
Rodrigo Alonso <rodrigo.alonso.o@gmail.com>
2023-12-04 19:54:03 -0300

    cachipun: agrega validadores y testplan dummy

---
+++ cachipun/attic/validators/st1.cpp
@@ -0,0 +1,22 @@
+#include "testlib.h"
+
+using namespace std;
+
+int main() {
+  registerValidation();
+  int n = inf.readInt(2, 2, "n");
+  inf.readSpace();
+  int k = inf.readInt(1, 3'000, "k");
+  inf.readEoln();
+
+  for (int i = 1; i <= k; i++) {
+    for (int j = 1; j <= n; j++) {
+      int d = inf.readInt(0, 2, "d_{" + to_string(i) + "," + to_string(j) + "}");
+      if (j != n) {
+        inf.readSpace();
+      }
+    }
+    inf.readEoln();
+  }
+  inf.readEof();
+}
