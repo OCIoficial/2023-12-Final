d60121a80c6527c61aca1577807120ecd99e4740
Rodrigo Alonso <rodrigo.alonso.o@gmail.com>
2023-12-04 19:40:09 -0300

    balancin: agrega validadores y testplan dummy

---
+++ balancin/attic/validators/st1.cpp
@@ -0,0 +1,14 @@
+#include "testlib.h"
+
+using namespace std;
+
+int main() {
+  registerValidation();
+  int n = inf.readInt(1, 1'000, "n");
+  inf.readSpace();
+  int c = inf.readInt(1, 1'000, "c");
+  inf.readSpace();
+  int g = inf.readInt(1, 1'000, "g");
+  inf.readEoln();
+  inf.readEof();
+}
